ad19df5638036d0964fc84c08209227a951fc5e3
halfstorm <halfstorm@nexigate.win>
2026-02-15 21:09:01 -0600

    Added configurable bindings for Throw and Throw Stack

---
+++ common/src/main/java/win/demistorm/client/RemapBindings.java
@@ -0,0 +1,29 @@
+package win.demistorm.client;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
+
+// Custom key mappings
+public class RemapBindings {
+
+    public static final KeyMapping.Category CATEGORY = KeyMapping.Category.register(
+        Identifier.fromNamespaceAndPath("vr_throwing_extensions", "category")
+    );
+
+    // Throw/catch
+    public static final KeyMapping THROW = new KeyMapping(
+        "key.vr_throwing_extensions.throw",
+            InputConstants.Type.MOUSE,
+            InputConstants.MOUSE_BUTTON_LEFT,
+            CATEGORY
+    );
+
+    // Throw stack/Null modifier
+    public static final KeyMapping THROW_STACK = new KeyMapping(
+        "key.vr_throwing_extensions.throw_stack",
+            InputConstants.Type.MOUSE,
+            InputConstants.MOUSE_BUTTON_RIGHT,
+            CATEGORY
+    );
+}
