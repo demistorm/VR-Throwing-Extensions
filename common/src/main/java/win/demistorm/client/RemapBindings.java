746e88a31eef861385ad8104b161f406c261cdde
halfstorm <halfstorm@nexigate.win>
2026-03-14 14:50:18 -0500

    Solved rebindable keybind issue!
    # Conflicts:
    #	common/src/main/java/win/demistorm/client/RemapBindings.java

--- common/src/main/java/win/demistorm/client/RemapBindings.java
+++ common/src/main/java/win/demistorm/client/RemapBindings.java
@@ -1,8 +1,8 @@
 package win.demistorm.client;
 
-import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.resources.Identifier;
+import net.minecraft.client.Minecraft;
 
 // Custom key mappings
 public class RemapBindings {
@@ -13,24 +13,34 @@ public class RemapBindings {
 
     // Throw/catch (main hand)
     public static final KeyMapping THROW = new KeyMapping(
-        "key.vr_throwing_extensions.throw",
-            InputConstants.Type.MOUSE,
-            InputConstants.MOUSE_BUTTON_LEFT,
+            "key.vr_throwing_extensions.throw",
+            -1,  // Uses vanilla left click unless rebound
             CATEGORY
     );
 
     // Throw stack/Null modifier
     public static final KeyMapping THROW_STACK = new KeyMapping(
-        "key.vr_throwing_extensions.throw_stack",
-            InputConstants.Type.MOUSE,
-            InputConstants.MOUSE_BUTTON_RIGHT,
+            "key.vr_throwing_extensions.throw_stack",
+            -1,  // Uses vanilla right click unless rebound
             CATEGORY
     );
 
     // Offhand throw/catch
     public static final KeyMapping THROW_OFFHAND = new KeyMapping(
-        "key.vr_throwing_extensions.throw_offhand",
+            "key.vr_throwing_extensions.throw_offhand",
             -1,
             CATEGORY
     );
+
+    // Returns custom binding if rebound, otherwise default attack key
+    public static boolean isThrowPressed() {
+        return !THROW.isDefault() ? THROW.isDown()
+                : Minecraft.getInstance().options.keyAttack.isDown();
+    }
+
+    // Returns custom binding if rebound, otherwise default use key
+    public static boolean isThrowStackPressed() {
+        return !THROW_STACK.isDefault() ? THROW_STACK.isDown()
+                : Minecraft.getInstance().options.keyUse.isDown();
+    }
 }
